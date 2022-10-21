beb46c2598e2dfac117b2289add54080cd79ecb0
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2022-10-20 20:11:17 -0500

    test

---
+++ src/extensions/jg_files/test.js
@@ -0,0 +1,54 @@
+class YourExt {
+    constructor(runtime, id) {
+        //ext stuff
+        this.runtime = runtime;
+        this.menuIconURI = null;
+        this.blockIconURI = null;
+        this.colorScheme = ["#41e2d0", "#0DA57A"];
+    }
+    getInfo() {
+        return {
+            id: "gameutils",
+            name: "GameUtils",
+            blockIconURI: this.blockIconURI,
+            menuIconURI: this.menuIconURI,
+            color1: this.colorScheme[0],
+            color2: this.colorScheme[1],
+            blocks: [{
+                "opcode": "LoadProject",
+                "blockType": "command",
+                "text": "Load Project from [url]",
+                "arguments": {
+                    "url": {
+                        "type": "string",
+                        "defaultValue": ""
+                    }
+                }
+            }, ]
+        }
+    }
+    async LoadProject(args, util) {
+        try {
+            const req = await fetch(args.ul)
+            if (req.status == 200) {
+                vm.loadProject(await req.blob())
+            } else {
+                console.error("Could not load project")
+                }
+            } catch (e) {
+                console.error(e)
+            }
+        }
+    }
+    (function() {
+        var extensionClass = YourExt;
+        if (typeof window === "undefined" || !window.vm) {
+            Scratch.extensions.register(new extensionClass());
+            console.log("YourExt loaded. Detecting sandboxed mode, performance will suffer. Please load YourExt in Unsandboxed mode.");
+        } else {
+            var extensionInstance = new extensionClass(window.vm.extensionManager.runtime);
+            var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+            window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+            console.log("YourExt loaded. Detecting unsandboxed mode.");
+        };
+    })()
