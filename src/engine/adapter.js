af6505bdef1029f1d02fdc4622f6cad45577c1c7
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-18 02:56:27 -0800

    forgor

--- src/engine/adapter.js
+++ src/engine/adapter.js
@@ -30,8 +30,8 @@ const domToBlock = function (blockDOM, blocks, isTopBlock, parent) {
         topLevel: isTopBlock, // If this block starts a stack.
         parent: parent, // Parent block ID, if available.
         shadow: blockDOM.name === 'shadow', // If this represents a shadow/slot.
-        x: blockDOM.attributes.x.value ??= null, // X position of script, if top-level.
-        y: blockDOM.attributes.y.value ??= null // Y position of script, if top-level.
+        x: blockDOM.attributes.x.value, // X position of script, if top-level.
+        y: blockDOM.attributes.y.value // Y position of script, if top-level.
     };
 
     // Add the block to the representation tree.
