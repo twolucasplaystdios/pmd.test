5173587688f67b9fe868bcd73b14c88ceb0c63fa
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2022-12-27 18:07:48 -0800

    enabled loading extensions from save

--- src/extension-support/tw-security-manager.js
+++ src/extension-support/tw-security-manager.js
@@ -37,7 +37,8 @@ class SecurityManager {
      */
     canLoadExtensionFromProject (extensionURL) {
         // Default to false for security
-        return Promise.resolve(false);
+        // set to true so that custom extensions can be used
+        return Promise.resolve(true);
     }
 }
 
