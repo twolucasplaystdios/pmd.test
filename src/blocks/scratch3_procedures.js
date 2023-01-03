15e5d6cbbb08a43a290bd738b01be8b58828330c
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-03 11:17:57 -0600

    Hhhh

--- src/blocks/scratch3_procedures.js
+++ src/blocks/scratch3_procedures.js
@@ -1,5 +1,6 @@
+const Cast = require('../util/cast');
 class Scratch3ProcedureBlocks {
-    constructor(runtime) {
+    constructor (runtime) {
         /**
          * The runtime instantiating this block package.
          * @type {Runtime}
@@ -11,69 +12,56 @@ class Scratch3ProcedureBlocks {
      * Retrieve the block primitives implemented by this package.
      * @return {object.<string, Function>} Mapping of opcode to Function.
      */
-    getPrimitives() {
+    getPrimitives () {
         return {
             procedures_definition: this.definition,
+            procedures_definition_return: this.definition,
             procedures_call: this.call,
+            procedures_call_return: this.call,
+            procedures_return: this.return,
             argument_reporter_string_number: this.argumentReporterStringNumber,
             argument_reporter_boolean: this.argumentReporterBoolean
         };
     }
 
-    definition() {
+    definition () {
         // No-op: execute the blocks.
     }
 
-    call(args, util) {
-        if (!util.stackFrame.executed) {
-            const procedureCode = args.mutation.proccode;
-            const paramNamesIdsAndDefaults = util.getProcedureParamNamesIdsAndDefaults(procedureCode);
+    call (args, util) {
+        const procedureCode = args.mutation.proccode;
+        const paramNamesIdsAndDefaults = util.getProcedureParamNamesIdsAndDefaults(procedureCode);
 
-            // If null, procedure could not be found, which can happen if custom
-            // block is dragged between sprites without the definition.
-            // Match Scratch 2.0 behavior and noop.
-            if (paramNamesIdsAndDefaults === null) {
-                return;
+        // If null, procedure could not be found, which can happen if custom
+        // block is dragged between sprites without the definition.
+        // Match Scratch 2.0 behavior and noop.
+        if (paramNamesIdsAndDefaults === null) return;
+
+        const [paramNames, paramIds, paramDefaults] = paramNamesIdsAndDefaults;
+
+        // Initialize params for the current stackFrame to {}, even if the procedure does
+        // not take any arguments. This is so that `getParam` down the line does not look
+        // at earlier stack frames for the values of a given parameter (#1729)
+        util.initParams();
+        for (let i = 0; i < paramIds.length; i++) {
+            if (args.hasOwnProperty(paramIds[i])) {
+                util.pushParam(paramNames[i], args[paramIds[i]]);
+            } else {
+                util.pushParam(paramNames[i], paramDefaults[i]);
             }
-
-            const [paramNames, paramIds, paramDefaults] = paramNamesIdsAndDefaults;
-
-            // Initialize params for the current stackFrame to {}, even if the procedure does
-            // not take any arguments. This is so that `getParam` down the line does not look
-            // at earlier stack frames for the values of a given parameter (#1729)
-            util.initParams();
-            for (let i = 0; i < paramIds.length; i++) {
-                if (args.hasOwnProperty(paramIds[i])) {
-                    util.pushParam(paramNames[i], args[paramIds[i]]);
-                } else {
-                    util.pushParam(paramNames[i], paramDefaults[i]);
-                }
-            }
-
-            const addonBlock = util.runtime.getAddonBlock(procedureCode);
-            if (addonBlock) {
-                const result = addonBlock.callback(util.thread.getAllparams(), util);
-                if (util.thread.status === 1 /* STATUS_PROMISE_WAIT */) {
-                    // If the addon block is using STATUS_PROMISE_WAIT to force us to sleep,
-                    // make sure to not re-run this block when we resume.
-                    util.stackFrame.executed = true;
-                }
-                return result;
-            }
-
-            util.stackFrame.executed = true;
-
-            util.startProcedure(procedureCode);
         }
+        
+        util.startProcedure(procedureCode);
     }
 
-    argumentReporterStringNumber(args, util) {
+    return (args, util) {
+        util.pushReportedValue(args.VALUE);
+        util.stopThisScript();
+    }
+
+    argumentReporterStringNumber (args, util) {
         const value = util.getParam(args.VALUE);
         if (value === null) {
-            // tw: support legacy block
-            if (String(args.VALUE).toLowerCase() === 'last key pressed') {
-                return util.ioQuery('keyboard', 'getLastKeyPressed');
-            }
             // When the parameter is not found in the most recent procedure
             // call, the default is always 0.
             return 0;
@@ -81,17 +69,9 @@ class Scratch3ProcedureBlocks {
         return value;
     }
 
-    argumentReporterBoolean(args, util) {
+    argumentReporterBoolean (args, util) {
         const value = util.getParam(args.VALUE);
         if (value === null) {
-            // tw: implement is compiled? and is turbowarp?
-            const lowercaseValue = String(args.VALUE).toLowerCase();
-            if (util.target.runtime.compilerOptions.enabled && lowercaseValue === 'is compiled?') {
-                return true;
-            }
-            if (lowercaseValue === 'is turbowarp?' || lowercaseValue === 'is penguinmod or turbowarp?') {
-                return true;
-            }
             // When the parameter is not found in the most recent procedure
             // call, the default is always 0.
             return 0;
