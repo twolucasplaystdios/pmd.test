513639d1095e5cd5aff163b8e034e41eaea34f5d
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-08 20:42:29 -0800

    EEE

--- src/extensions/jg_runtime/index.js
+++ src/extensions/jg_runtime/index.js
@@ -181,7 +181,7 @@ class JgRuntimeBlocks {
     }
     addCostumeUrl(args, util) {
         fetch(args.URL, { mode: 'no-cors' }).then(x => x.blob().then(blob => {
-            console.log(x.get('type'))
+            console.log(x)
             if (!(blob.type !== 'image/png' || blob.type !== 'image/jpg' || blob.type !== 'image/svg+xml')) throw new Error('invalid mime type: '+blob.type)
             const assetType = blob.type !== 'image/png' || blob.type !== 'image/jpg' 
                 ? this.runtime.storage.AssetType.ImageBitmap 
