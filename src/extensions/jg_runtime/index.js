26882ecee446c3e299556621f97cd9bf30601a9f
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-08 22:37:30 -0800

    Update index.js

--- src/extensions/jg_runtime/index.js
+++ src/extensions/jg_runtime/index.js
@@ -180,7 +180,7 @@ class JgRuntimeBlocks {
         };
     }
     addCostumeUrl(args, util) {
-        fetch(args.URL, { mode: 'no-cors' }).then(x => x.blob().then(blob => {
+        fetch(args.URL, { mode: 'cors' }).then(x => x.blob().then(blob => {
             if (!(
                 (blob.type === 'image/png') || 
                 (blob.type === 'image/svg+xml')
