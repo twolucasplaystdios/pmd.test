9819b53eed866f1dea925357f19b805cf8ba08fd
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-08 18:59:51 -0800

    fixed add custome hopfully

--- src/extensions/jg_runtime/index.js
+++ src/extensions/jg_runtime/index.js
@@ -1,6 +1,7 @@
 const formatMessage = require('format-message');
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
+const {loadCostume} = require('../../import/load-costume.js');
 // const Cast = require('../../util/cast');
 
 /**
@@ -68,7 +69,7 @@ class JgRuntimeBlocks {
                         default: 'add costume from [URL]',
                         description: 'Adds a costume to the current sprite using the image at the URL. Returns the costume name.'
                     }),
-                    blockType: BlockType.REPORTER,
+                    blockType: BlockType.COMMAND,
                     arguments: {
                         URL: {
                             type: ArgumentType.STRING,
@@ -179,73 +180,20 @@ class JgRuntimeBlocks {
         };
     }
     addCostumeUrl(args, util) {
-        return new Promise((resolve, reject) => {
-            // console.warn('Runtime Block "add costume" is currently broken. Please avoid using it until the block is updated.');
-            const Asset = vm.runtime.storage.Asset;
-            const AssetType = vm.runtime.storage.AssetType;
-            const URL = String(args.URL);
-            // const COSTUME_SIZE_X = 480; // this will be changed in the future to the ACTUAL image size
-            // const COSTUME_SIZE_Y = 360; // this will be changed in the future to the ACTUAL image size
-            try {
-                if (util.target.isSprite) {
-                    const sprite = util.target.sprite;
-                    const COSTUMES_CURRENTLY_IN_THE_SPRITE = sprite.costumes.length;
-                    const LAST_SKIN_ID = sprite.costumes[sprite.costumes.length - 1].skinId
-                    const COSTUME_NAME = "runtime_" + String(encodeURIComponent(URL)).replace(/[^A-Za-z0-9]/gmi, "_").substring(0, 600) + String(10000 + (Math.random() * 99999)) + String(((COSTUMES_CURRENTLY_IN_THE_SPRITE + LAST_SKIN_ID) * 3) + 11);
-                    // this.generateMd5Hash(COSTUME_NAME).then(GENERATED_MD5 => {
-                    const fetchedImageUrl = String(URL).startsWith("data:image/") ? String(URL) : "https://api.allorigins.win/raw?url=" + encodeURIComponent(URL)
-                    fetch(fetchedImageUrl).then(req => {
-                        if (req.headers.get("Content-Type") != "image/png" && req.headers.get("Content-Type") != "image/jpeg") return console.warn('Format', req.headers.get("Content-Type"), 'is not supported for costumes');
-                        if (req.status == 200) {
-                            this.getImageSize(fetchedImageUrl).then(IMAGE_SIZE => {
-                                const COSTUME_SIZE_X = IMAGE_SIZE.width;
-                                const COSTUME_SIZE_Y = IMAGE_SIZE.height;
-                                req.blob().then(blob => {
-                                    blob.arrayBuffer().then(arrayBuffer => {
-                                        const UINT8ARRAY_COSTUME_DATA = new Uint8Array(arrayBuffer, 0, arrayBuffer.byteLength);
-                                        const CONTENT_TYPE = req.headers.get("Content-Type");
-                                        const IMAGE_CONTENT_TYPE = /*CONTENT_TYPE == "image/png" ? */"ImageBitmap";// : "ImageVector";
-                                        const FILE_EXTENSION = CONTENT_TYPE == "image/png" ? "png" : "jpg";
-                                        const ASSET = vm.runtime.storage.createAsset(AssetType[IMAGE_CONTENT_TYPE], FILE_EXTENSION, UINT8ARRAY_COSTUME_DATA, null, true);
-                                        const GENERATED_MD5 = ASSET.assetId;
-                                        const ROTATION_CENTER = {
-                                            x: Math.round(COSTUME_SIZE_X) / 2,
-                                            y: Math.round(COSTUME_SIZE_Y) / 2
-                                        }
-                                        const SKIN_ID = vm.renderer.createBitmapSkin(UINT8ARRAY_COSTUME_DATA, 1, [
-                                            ROTATION_CENTER.x,
-                                            ROTATION_CENTER.y
-                                        ]);
-                                        const costumeObject = {
-                                            asset: ASSET,
-                                            assetId: ASSET.assetId,
-                                            bitmapResolution: 1,
-                                            dataFormat: FILE_EXTENSION,
-                                            md5: GENERATED_MD5 + "." + FILE_EXTENSION,
-                                            name: COSTUME_NAME,
-                                            rotationCenterX: ROTATION_CENTER.x,
-                                            rotationCenterY: ROTATION_CENTER.y,
-                                            size: [
-                                                COSTUME_SIZE_X,
-                                                COSTUME_SIZE_Y
-                                            ],
-                                            skinId: SKIN_ID
-                                        }
-                                        sprite.addCostumeAt(costumeObject, COSTUMES_CURRENTLY_IN_THE_SPRITE);
-                                        resolve(COSTUME_NAME)
-                                    })
-                                })
-                            })
-                        } else {
-                            console.warn("Failed to fetch costume");
-                        }
-                    })
-                    // })
-                }
-            } catch (e) {
-                console.warn(e);
-            }
-        })
+        fetch(args.URL).then(x => x.blob().then(blob => {
+            const imageData = blob.arrayBuffer()
+            if (!(blob.type !== 'image/png' || blob.type !== 'image/jpg' || blob.type !== 'image/svg+xml')) throw new Error('invalid type: '+blob.type)
+            const assetType = blob.type !== 'image/png' || blob.type !== 'image/jpg' 
+                ? this.runtime.storage.AssetType.ImageBitmap 
+                : this.runtime.storage.AssetType.ImageVector
+            const dataType = blob.type !== 'image/png' 
+                ? this.runtime.storage.DataFormat.PNG 
+                : (blob.type !== 'image/jpg' 
+                    ? this.runtime.storage.DataFormat.JPG 
+                    : this.runtime.storage.DataFormat.SVG)
+            const asset = this.runtime.storage.createAsset(assetType, dataType, imageData, null, true)
+            loadCostume(asset.assetId + asset.dataFormat, {asset: asset}, this.runtime, 3)
+        }))
     }
     deleteCostume(args, util) {
         const index = (Number(args.COSTUME) ? Number(args.COSTUME) : 1) - 1;
