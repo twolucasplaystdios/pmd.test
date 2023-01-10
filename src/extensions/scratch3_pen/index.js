921baa42eb5ad006d2e4fd83fe88a6dc051d7c55
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-09 18:08:18 -0800

    hmm

--- src/extensions/scratch3_pen/index.js
+++ src/extensions/scratch3_pen/index.js
@@ -1038,11 +1038,13 @@ class Scratch3PenBlocks {
             if (args.OPTION === LayerParam.FRONT) {
                 console.log('setting the layer order to', StageLayering.LAYER_GROUPS_PEN)
                 this.runtime.renderer.setLayerGroupOrdering(StageLayering.LAYER_GROUPS_PEN);
-                //this.runtime.renderer.setDrawableOrder(this._penDrawableId, Infinity, StageLayering.PEN_LAYER);
+                this._getPenLayerID()
+                this.runtime.renderer.setDrawableOrder(this._penDrawableId, Infinity, StageLayering.PEN_LAYER);
             } else if (args.OPTION === LayerParam.BACK) {
                 console.log('setting the layer order to', StageLayering.LAYER_GROUPS)
                 this.runtime.renderer.setLayerGroupOrdering(StageLayering.LAYER_GROUPS);
-                //this.runtime.renderer.setDrawableOrder(this._penDrawableId, -Infinity, StageLayering.PEN_LAYER);
+                this._getPenLayerID()
+                this.runtime.renderer.setDrawableOrder(this._penDrawableId, -Infinity, StageLayering.PEN_LAYER);
             }
         }
     }
