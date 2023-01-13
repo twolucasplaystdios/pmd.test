897c7e51def5bb94ffc2134ed48c77a713414513
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-13 01:29:41 -0800

    Update jsgen.js

--- src/compiler/jsgen.js
+++ src/compiler/jsgen.js
@@ -931,13 +931,13 @@ class JSGenerator {
             this.source += `target.setSize(${this.descendInput(node.size).asNumber()});\n`;
             break;
         case 'looks.setFont':
-            this.source += `runtime.ext_scratch3_looks.setFont({ font: ${this.descendInput(node.font).asString()}, size: ${this.descendInput(node.size).asNumber()} });\n`
+            this.source += `runtime.ext_scratch3_looks.setFont({ font: ${this.descendInput(node.font).asString()}, size: ${this.descendInput(node.size).asNumber()} }, { target: target });\n`
             break;
         case 'looks.setColor':
-            this.source += `runtime.ext_scratch3_looks.setColor({ prop: "${sanitize(node.prop)}", size: ${this.descendInput(node.color).asColor()} });\n`
+            this.source += `runtime.ext_scratch3_looks.setColor({ prop: "${sanitize(node.prop)}", size: ${this.descendInput(node.color).asColor()} }, { target: target });\n`
             break;
         case 'looks.setShape':
-            this.source += `runtime.ext_scratch3_looks.setShape({ prop: "${sanitize(node.prop)}", size: ${this.descendInput(node.color).asColor()} });\n`
+            this.source += `runtime.ext_scratch3_looks.setShape({ prop: "${sanitize(node.prop)}", size: ${this.descendInput(node.color).asColor()} }, { target: target });\n`
             break;
         case 'looks.show':
             this.source += 'target.setVisible(true);\n';
