44ebcab574566dd6eb66fdbd224df50fec305d4f
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-12 18:37:36 -0800

    well fine then be like that

--- src/blocks/scratch3_looks.js
+++ src/blocks/scratch3_looks.js
@@ -28,7 +28,6 @@ class Scratch3LooksBlocks {
         this._onResetBubbles = this._onResetBubbles.bind(this);
         this._onTargetWillExit = this._onTargetWillExit.bind(this);
         this._updateBubble = this._updateBubble.bind(this);
-        this.styleDefault = this.runtime.renderer.getBubbleDefaults()
 
         // Reset all bubbles on start/stop
         this.runtime.on('PROJECT_STOP_ALL', this._onResetBubbles);
@@ -50,7 +49,27 @@ class Scratch3LooksBlocks {
             text: '',
             type: 'say',
             usageId: null,
-            props: this.styleDefault
+            // @todo make this read from renderer
+            props: {
+                MAX_LINE_WIDTH: 170, // Maximum width, in Scratch pixels, of a single line of text
+            
+                MIN_WIDTH: 50, // Minimum width, in Scratch pixels, of a text bubble
+                STROKE_WIDTH: 4, // Thickness of the stroke around the bubble. Only half's visible because it's drawn under the fill
+                PADDING: 10, // Padding around the text area
+                CORNER_RADIUS: 16, // Radius of the rounded corners
+                TAIL_HEIGHT: 12, // Height of the speech bubble's "tail". Probably should be a constant.
+            
+                FONT: 'Helvetica', // Font to render the text with
+                FONT_SIZE: 14, // Font size, in Scratch pixels
+                FONT_HEIGHT_RATIO: 0.9, // Height, in Scratch pixels, of the text, as a proportion of the font's size
+                LINE_HEIGHT: 16, // Spacing between each line of text
+            
+                COLORS: {
+                    BUBBLE_FILL: 'white',
+                    BUBBLE_STROKE: 'rgba(0, 0, 0, 0.15)',
+                    TEXT_FILL: '#575E75'
+                }
+            }
         };
     }
 
