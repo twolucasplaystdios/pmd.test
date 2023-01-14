17374d0aa2c4d1561886da9c4f2840826490c5ab
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-14 12:21:41 -0600

    Update extension-manager.js

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -1,4 +1,4 @@
-/*jshint -W024, esversion : 11, node : true, asi : true, loopfunc : true*/
+/*jshint esversion : 11, node : true, asi : true, loopfunc : true*/
 const dispatch = require('../dispatch/central-dispatch');
 const log = require('../util/log');
 const maybeFormatMessage = require('../util/maybe-format-message');
@@ -638,8 +638,8 @@ class ExtensionManager {
         }
         return extensionURLs;
     }
-    for (let S of injectExtensions) {
-  		injectExtensions[S] = () => extension;
+    for (II = 0; II < injectExtensions.length; II++) {
+  		injectExtensions[II] = () => extension;
 		}
 }
 
