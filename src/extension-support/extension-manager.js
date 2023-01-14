f78e0b3793ecddb114d3c7c305c78713b4421296
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-14 13:53:22 -0800

    Revert "Update extension-manager.js"
    This reverts commit f17485dafde9323c6b22fdded821cb59624ecfc9.

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -1,4 +1,3 @@
-/*jshint esversion : 11, node : true, asi : true, loopfunc : true*/
 const dispatch = require('../dispatch/central-dispatch');
 const log = require('../util/log');
 const maybeFormatMessage = require('../util/maybe-format-message');
@@ -69,7 +68,7 @@ const builtinExtensions = {
     // text: text stuff
     GamepadExtension: () => require("../extensions/GamepadExtension"),
 };
-const injectExtensions = [builtinExtensions.jwProto];
+
 /**
  * @typedef {object} ArgumentInfo - Information about an extension block argument
  * @property {ArgumentType} type - the type of value this argument can take
@@ -637,9 +636,6 @@ class ExtensionManager {
             }
         }
         return extensionURLs;
-    }
-    for (let S = 0; S < injectExtensions.length; S++) {
-        injectExtensions[S] = () => extension;
     }
 }
 
