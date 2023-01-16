daef9fe663abdd749d788ef2b97d0f2673a8f85c
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-15 22:18:56 -0800

    Update runtime.js

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1566,7 +1566,8 @@ class Runtime extends EventEmitter {
      */
     getBlocksXML (target) {
         return this._blockInfo.map(categoryInfo => {
-            const {name, color1, color2, orderBlocks} = categoryInfo;
+            const {name, color1, color2} = categoryInfo;
+            let {orderBlocks} = categoryInfo
             // Filter out blocks that aren't supposed to be shown on this target, as determined by the block info's
             // `hideFromPalette` and `filter` properties.
             const paletteBlocks = categoryInfo.blocks.filter(block => {
