504ad7665bb5275391d1ea04b9da495ff7e6a52a
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-15 17:05:02 -0800

    ohhhhhhh god damit

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -321,11 +321,11 @@ class ExtensionManager {
      * @returns {Promise} resolved once all the extensions have been reinitialized
      */
     refreshDynamicCategorys() {
-        if (!this._loadedExtensions) return Promise.reject('_loadedExtensions is not readable yet')
-        const allPromises = Array.from(this._loadedExtensions.values()).map(serviceName =>
+        if (!ExtensionManager._loadedExtensions) return Promise.reject('_loadedExtensions is not readable yet')
+        const allPromises = Array.from(ExtensionManager._loadedExtensions.values()).map(serviceName =>
             dispatch.call(serviceName, 'getInfo')
                 .then(info => {
-                    info = this._prepareExtensionInfo(serviceName, info);
+                    info = ExtensionManager._prepareExtensionInfo(serviceName, info);
                     if (!info.isDynamic) return
                     dispatch.call('runtime', '_refreshExtensionPrimitives', info);
                 })
