831f78a180d1b5ddff70abfded489e054695ba7a
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-16 22:00:39 -0800

    Update jsgen.js

--- src/compiler/jsgen.js
+++ src/compiler/jsgen.js
@@ -1239,6 +1239,7 @@ class JSGenerator {
             const field = node.fields[fieldName];
             result += `"${sanitize(fieldName)}":"${sanitize(field)}",`;
         }
+        result += `"mutation":${JSON.stringify(node.mutation)},`;
         const opcodeFunction = this.evaluateOnce(`runtime.getOpcodeFunction("${sanitize(opcode)}")`);
         result += `}, ${opcodeFunction}, ${this.isWarp}, ${setFlags}, null)`;
 
