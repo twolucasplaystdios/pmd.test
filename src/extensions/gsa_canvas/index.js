41725696d5ed4893b038e61c0e6e648087175aee
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-18 16:36:20 -0800

    Update index.js

--- src/extensions/gsa_canvas/index.js
+++ src/extensions/gsa_canvas/index.js
@@ -1,46 +1,46 @@
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const cstore = require('./canvasStorage');
-const store = new cstore()
+const store = new cstore();
 
 /**
  * Class
  * @constructor
  */
 class canvas {
-    constructor(runtime) {
+    constructor (runtime) {
         /**
          * The runtime instantiating this block package.
          * @type {runtime}
          */
         this.runtime = runtime;
-        store.attachRuntime(runtime)
+        store.attachRuntime(runtime);
     }
 
     orderCategoryBlocks (blocks) {
-        const button = blocks[0]
-        const varBlock = blocks[1]
-        delete blocks[0]
-        delete blocks[1]
+        const button = blocks[0];
+        const varBlock = blocks[1];
+        delete blocks[0];
+        delete blocks[1];
         // create the variable block xml's
         const varBlocks = store.getAllCanvases().map(canvas => varBlock
-                .replace('{canvasId}', canvas.id)
-                .replace('{canvasName}', canvas.name))
+            .replace('{canvasId}', canvas.id)
+            .replace('{canvasName}', canvas.name));
         // push the button to the top of the var list
         varBlocks
             .reverse()
-            .push(button)
+            .push(button);
         // merge the category blocks and variable blocks into one block list
         blocks = varBlocks
             .reverse()
-            .concat(blocks)
-        return blocks
+            .concat(blocks);
+        return blocks;
     }
 
     /**
      * @returns {object} metadata for this extension and its blocks.
      */
-    getInfo() {
+    getInfo () {
         return {
             id: 'canvas',
             name: 'html canvas',
@@ -54,14 +54,14 @@ class canvas {
                     opcode: 'createNewCanvas',
                     blockType: BlockType.BUTTON,
                     text: 'create new canvas'
-                }, 
+                },
                 {
                     opcode: 'canvasGetter',
                     blockType: BlockType.REPORTER,
                     isDynamic: true,
                     canvasId: '{canvasId}',
                     text: '{canvasName}'
-                }, 
+                },
                 "---",
                 {
                     opcode: 'printCanvas',
@@ -73,7 +73,7 @@ class canvas {
                         }
                     },
                     text: 'stamp canvas [canvas] to pen'
-                }, 
+                },
                 {
                     blockType: BlockType.LABEL,
                     text: "2D"
@@ -88,42 +88,41 @@ class canvas {
                         }
                     },
                     text: 'is json [json] valid?'
-                }, 
+                },
                 {
                     blockType: BlockType.LABEL,
                     text: "3D"
-                },
+                }
             ],
             menus: {
                 canvas: 'getCanvasMenuItems'
-            },
+            }
         };
     }
 
-    createNewCanvas(workspace) {
-        const name = window.prompt('canvas name', 'my canvas')
-        store.newCanvas(name, this.runtime.stageWidth, this.runtime.stageHeight)
-        vm.emitWorkspaceUpdate()
+    createNewCanvas () {
+        const name = window.prompt('canvas name', 'my canvas');
+        store.newCanvas(name, this.runtime.stageWidth, this.runtime.stageHeight);
+        // eslint-disable-next-line no-undef
+        vm.emitWorkspaceUpdate();
     }
 
-    getCanvasMenuItems() {
-        const canvases = store.getAllCanvases()
-        if (canvases.length < 1) return [{text: '', value: ''}]
-        return canvases.map(canvas => {
-            return {
-                text: canvas.name,
-                value: canvas.id
-            }
-        })
+    getCanvasMenuItems () {
+        const canvases = store.getAllCanvases();
+        if (canvases.length < 1) return [{text: '', value: ''}];
+        return canvases.map(canvas => ({
+            text: canvas.name,
+            value: canvas.id
+        }));
     }
 
-    canvasGetter(args, util, mutation) {
-        return store.getCanvas(mutation.canvasId).element.toDataURL('image/png')
+    canvasGetter (args, util, mutation) {
+        return store.getCanvas(mutation.canvasId).element.toDataURL('image/png');
     }
 
-    printCanvas(args) {
-        const penSkinId = this.runtime.renderer.getPenDrawableId()
-        const canvas = store.getCanvas(args.canvas)
+    printCanvas (args) {
+        const penSkinId = this.runtime.renderer.getPenDrawableId();
+        const canvas = store.getCanvas(args.canvas);
         this.runtime.renderer.penStamp(penSkinId, canvas.drawableId);
         this.runtime.requestRedraw();
     }
