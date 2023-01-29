d29ae0334bfead07a3c4e2017449d21a452fb78e
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-28 19:31:13 -0800

    canvas stuff?

--- src/extensions/gsa_canvas/index.js
+++ src/extensions/gsa_canvas/index.js
@@ -17,6 +17,108 @@ class canvas {
         store.attachRuntime(runtime);
     }
 
+    readAsImageElement (src) {
+        return new Promise((resolve, reject) => {
+            const image = new Image();
+            image.onload = function () {
+                resolve(image);
+                image.onload = null;
+                image.onerror = null;
+            };
+            image.onerror = function () {
+                reject(new Error('Costume load failed. Asset could not be read.'));
+                image.onload = null;
+                image.onerror = null;
+            };
+            image.src = src;
+        });
+    }
+
+    spawnCreateCanvasMenu (name) {
+        const modal = document.createElement('div');
+        modal.id = 'new-canvas-modal';
+        modal.innerHTML = `
+        <div 
+            class="ReactModal__Content ReactModal__Content--after-open modal_modal-content_1h3ll prompt_modal-content_1BfWj" 
+            tabindex="-1" 
+            role="dialog" 
+            aria-label="New Variable"
+        >
+            <div class="box_box_2jjDp" dir="ltr" style="flex-direction: column; flex-grow: 1;">
+                <div class="modal_header_1h7ps">
+                    <div class="modal_header-item_2zQTd modal_header-item-title_tLOU5">New Canvas</div>
+                    <div class="modal_header-item_2zQTd modal_header-item-close_2XDeL">
+                        <div 
+                            aria-label="Close" 
+                            class="close-button_close-button_lOp2G close-button_large_2oadS" 
+                            role="button" 
+                            tabindex="0"
+                        >
+                            <img 
+                                id="canvas-creator-close" 
+                                class="close-button_close-icon_HBCuO" 
+                                src="static/assets/cb666b99d3528f91b52f985dfb102afa.svg"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="prompt_body_18Z-I box_box_2jjDp">
+                    <div class="prompt_label_tWjYZ box_box_2jjDp">New canvas name:</div>
+                    <div class="box_box_2jjDp">
+                        <input 
+                            id="canvas-creator-name" 
+                            class="prompt_variable-name-text-input_1iu8-" 
+                            name="New canvas name:" 
+                            value="${name ? name : 'My Canvas'}"
+                        />
+                        <input id="canvas-creator-default" name="Default canvas content"/>
+                    </div>
+                    <div class="prompt_button-row_3Wc5Z box_box_2jjDp">
+                        <button id="canvas-creator-cancel" class="prompt_cancel-button_36cPC">
+                            <span>Cancel</span>
+                        </button>
+                        <button id="canvas-creator-ok" class="prompt_ok-button_3QFdD">
+                            <span>OK</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        const closeFunc = () => {
+            modal.remove();
+        };
+        const okButton = document.getElementById("canvas-creator-ok");
+        const cancelButton = document.getElementById("canvas-creator-cancel");
+        const closeButton = document.getElementById("canvas-creator-close");
+        const nameInput = document.getElementById("canvas-creator-name");
+        const defaultImageInput = document.getElementById("canvas-creator-default");
+        const fileReader = new FileReader();
+        let defaultImage;
+        fileReader.onload = e => {
+            this.readAsImageElement(e.target.result).then(img => {
+                defaultImage = img;
+            });
+        };
+        okButton.onclick = () => {
+            store.newCanvas(nameInput.value, this.runtime.stageWidth, this.runtime.stageHeight, defaultImage);
+            // eslint-disable-next-line no-undef
+            vm.emitWorkspaceUpdate();
+        };
+        cancelButton.onclick = closeFunc;
+        closeButton.onclick = closeFunc;
+        defaultImageInput.onchange = () => {
+            const file = defaultImageInput.files[0];
+            if (!file) {
+                return;
+            } 
+            fileReader.readAsDataURL(file);
+        };
+        defaultImageInput.onblur = () => {
+            defaultImageInput.onchange();
+        };
+    }
+
     orderCategoryBlocks (blocks) {
         const button = blocks[0];
         const varBlock = blocks[1];
@@ -101,10 +203,7 @@ class canvas {
     }
 
     createNewCanvas () {
-        const name = window.prompt('canvas name', 'my canvas');
-        store.newCanvas(name, this.runtime.stageWidth, this.runtime.stageHeight);
-        // eslint-disable-next-line no-undef
-        vm.emitWorkspaceUpdate();
+        this.spawnCreateCanvasMenu();
     }
 
     getCanvasMenuItems () {
