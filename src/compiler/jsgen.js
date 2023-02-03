66bc58e1f7e629f1701938bb7ba4f232ff04ad32
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-02-02 21:39:35 -0800

    Update jsgen.js

--- src/compiler/jsgen.js
+++ src/compiler/jsgen.js
@@ -712,7 +712,7 @@ class JSGenerator {
                 number: TYPE_NUMBER,
                 boolean: TYPE_BOOLEAN
             };
-            const type = node.type.toLowerCase();
+            const type = JSON.parse(node.type) || 'string';
             const blockType = types[type];
             const procedureCode = node.code;
             const procedureVariant = node.variant;
