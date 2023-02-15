2e4905d3ab4434b7a71b890dac152cc9fb70befe
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-02-14 22:20:54 -0800

    Update runtime.js

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1546,7 +1546,7 @@ class Runtime extends EventEmitter {
 
             if (shadowType === 'polygon') {
                 // eslint-disable-next-line max-len
-                context.inputList.push(`<mutation points="${argInfo.nodes}" color="${context.blockJSON.colour}" midle="[0,0]" scale="${argInfo.defaultSize || 30}"/>`);
+                context.inputList.push(`<mutation expanded="false" points="${argInfo.nodes}" color="${context.blockJSON.colour}" midle="[0,0]" scale="${argInfo.defaultSize || 30}"/>`);
             }
 
             // A <field> displays a dynamic value: a user-editable text field, a drop-down menu, etc.
