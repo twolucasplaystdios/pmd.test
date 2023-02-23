2a10a9c1329d431175b5829e0e39faa6f5b7806d
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-02-22 20:06:14 -0800

    idk

---
+++ src/util/javascript-container.js
@@ -0,0 +1,58 @@
+const rotationStyleOptions = [
+    'left-right',
+    'don\'t rotate',
+    'all around'
+];
+
+class execute {
+    constructor (funcText, target, runtime) {
+        const thisData = this.getThisFuncs(target, runtime);
+        thisData.mySelf = new Function(funcText);
+    }
+    getThisFuncs (target, runtime) {
+        // all of the functions accessed by `this`
+        // must have there internal vars defined externaly
+        // so that the code can not access nor edit them
+        const thisFuncs = {
+            // extension blocks, cant really moderate this sadly
+            get extensions () {
+                const extensions = {};
+                const keys = Object.keys(runtime);
+                keys.filter(name => name.startsWith('ext_'))
+                    .forEach(name => {
+                        const extName = name.replace('ext_', '');
+                        extensions[extName] = runtime[name];
+                    });
+                return extensions;
+            },
+            goto: target.setXY,
+            set xPosition (newPos) {
+                target.setXY(newPos, target.y);
+            },
+            set yPosition (newPos) {
+                target.setXY(target.x, newPos);
+            },
+            get xPosition () {
+                return target.x;
+            },
+            get yPosition () {
+                return target.y;
+            },
+            set direction (newDir) {
+                target.setDirection(newDir);
+            },
+            get direction () {
+                return target.direction;
+            },
+            set rotationStyle (style) {
+                if (!rotationStyleOptions.includes(style)) {
+                    throw new Error(`invalid style: ${style}`);
+                }
+                target.setRotationStyle(style);
+            }
+        };
+        return thisFuncs;
+    }
+}
+
+module.exports = execute;
