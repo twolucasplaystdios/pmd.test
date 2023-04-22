8d62c718a688c6079d7a5885facd8b3d2e497640
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-04-22 00:59:34 -0600

    remove an old message from permissions

--- src/util/project-permissions.js
+++ src/util/project-permissions.js
@@ -18,7 +18,7 @@ class ProjectPermissionManager {
         limitedWebsite: "display $0",
     };
     static permissionDrawbacks = {
-        javascript: ["do literally anything in your browser"],
+        javascript: [],
         camera: ["enable or disable your video camera"],
         allWebsites: ["display websites on screen", "visit potentionally bypassed websites", "display websites made inside the project", "play audio, show pop-ups, or follow redirects inside of any sites visited"],
         cameraPictures: ["take a screenshot while your camera is visible"],
