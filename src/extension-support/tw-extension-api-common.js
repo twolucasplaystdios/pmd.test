0c76eed24fcc8c4b41dae42292aff105fb59b6a2
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-04-26 18:23:21 -0600

    Update tw-extension-api-common.js

--- src/extension-support/tw-extension-api-common.js
+++ src/extension-support/tw-extension-api-common.js
@@ -2,12 +2,16 @@ const ArgumentType = require('./argument-type');
 const BlockType = require('./block-type');
 const TargetType = require('./target-type');
 const Cast = require('../util/cast');
+const Clone = require('../util/clone');
+const Color = require('../util/color');
 
 const Scratch = {
     ArgumentType,
     BlockType,
     TargetType,
-    Cast
+    Cast,
+    Clone,
+    Color
 };
 
 module.exports = Scratch;
