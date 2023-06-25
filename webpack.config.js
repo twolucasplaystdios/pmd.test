87af5dbaac38f4ee131708ed85b16ce7be64e487
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-06-25 17:11:37 -0600

    Revert "mayb"
    This reverts commit 8ac9555c3402d358351bc6f4e737957f3c1704f4.

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ const base = {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
+            include: path.resolve(__dirname, '*'),
             query: {
                 presets: [['@babel/preset-env']]
             },
