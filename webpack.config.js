bdc3fa3849a22ba83f652a16cdd68bd88942228e
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-06-25 17:55:59 -0500

    h

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const base = {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
-            include: __dirname,
+            include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node-modulesgt')],
             query: {
                 presets: [['@babel/preset-env']]
             },
