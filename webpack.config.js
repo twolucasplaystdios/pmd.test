c36929042b4a73064e04397965712d41cf00b3d7
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-06-25 17:11:41 -0600

    Revert "fix mayb"
    This reverts commit 4cf687b9777be5ac8d9e5bb2cac041d86672a0a0.

--- webpack.config.js
+++ webpack.config.js
@@ -29,17 +29,7 @@ const base = {
             options: {
                 outputPath: 'media/music/'
             }
-        },
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-          }
-        ]
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-      }]
+        }]
     },
     plugins: []
 };
