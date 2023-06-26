5a4d36c33dd37dc3a9b39a4a4f8e79ce5dbd08d9
Ianyourgod <ianlouishawthorne@gmail.com>
2023-06-25 20:01:48 -0500

    maybe worky?

--- src/extensions/iyg_perlin_noise/index.js
+++ src/extensions/iyg_perlin_noise/index.js
@@ -30,42 +30,31 @@ class iygPerlin {
             color2: '#636363',
             blocks: [
                 {
-                    opcode: 'GenerateNoise',
-                    blockType: BlockType.COMMAND,
+                    opcode: 'GetNoise',
+                    blockType: BlockType.REPORTER,
                     text: formatMessage({
-                        id: 'iygPerlin.GenerateNoise',
-                        default: 'Generate noise with seed [SEED], octave [OCTAVE], width [width], height [height]',
-                        description: 'Generate noise with a specified seed and size.'
+                        id: 'iygPerlin.GetNoise',
+                        default: 'Get noise with seed [SEED] and octave [OCTAVE] at x [X], y [Y], and z [Z]',
+                        description: 'Get seeded noise at a specified x and y and z.'
                     }),
                     arguments: {
                         SEED: {
                             type: ArgumentType.NUMBER,
-                            defaultValue: 12345
+                            defaultValue: 123
                         },
                         OCTAVE: {
                             type: ArgumentType.NUMBER,
                             defaultValue: 4
                         },
-                        SIZE: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 256
-                        }
-                    }
-                },
-                {
-                    opcode: 'GetNoise',
-                    blockType: BlockType.REPORTER,
-                    text: formatMessage({
-                        id: 'iygPerlin.GetNoise',
-                        default: 'Get noise at x [X] y [Y]',
-                        description: 'Get noise at a specified x and y.'
-                    }),
-                    arguments: {
                         X: {
                             type: ArgumentType.NUMBER,
                             defaultValue: 0
                         },
                         Y: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        },
+                        Z: {
                             type: ArgumentType.NUMBER,
                             defaultValue: 0
                         }
@@ -75,55 +64,102 @@ class iygPerlin {
         };
     }
 
-    GenerateNoise(args, util) {
-        let seed = args.SEED;
-        let octave = args.OCTAVE;
-        let size = args.SIZE;
-        const lcg = (() => {
-            // Set to values from http://en.wikipedia.org/wiki/Numerical_Recipes
-            // m is basically chosen to be large (as it is the max period)
-            // and for its relationships to a and c
-            const m = 4294967296;
-            // a - 1 should be divisible by m's prime factors
-            const a = 1664525;
-            // c and m should be co-prime
-            const c = 1013904223;
-            let seed, z;
-            return {
-              setSeed(val) {
-                // pick a random seed if val is undefined or null
-                // the >>> 0 casts the seed to an unsigned 32-bit integer
-                z = seed = (val == null ? Math.random() * m : val) >>> 0;
-              },
-              getSeed() {
-                return seed;
-              },
-              rand() {
-                // define the recurrence relationship
-                z = (a * z + c) % m;
-                // return a float in [0, 1)
-                // if z = m then z / m = 0 therefore (z % m) / m < 1 always
-                return z / m;
-              }
-            };
-          })();
-        
-          lcg.setSeed(seed);
-          this.noise = new Array(size);
-          for (let i = 0; i < size; i++) {
-            this.noise[i] = lcg.rand();
-          }
-
+    dumbSeedRandom(seed) {
+        x = (171 * seed) % 30269
+        y = (172 * seed) % 30307
+        z = (170 * seed) % 30323
+        return (x/30269.0 + y/30307.0 + z/30323.0) % 1.0
     }
 
     GetNoise(args, util) {
-        if (!this.noise || isNaN(args.X) || isNaN(args.Y)) {
-            return 0;
-        }
+        let seed = args.SEED;
+        let perlin_octaves = args.OCTAVE;
         let x = args.X;
         let y = args.Y;
-        console.log(this.noise, x, y);
-        return this.noise[x][y];
+        let z = args.Z;
+
+        let perlin_amp_falloff = 0.5;
+        const scaled_cosine = i => 0.5 * (1.0 - Math.cos(i * Math.PI));
+        const PERLIN_SIZE = 4095;
+        const PERLIN_YWRAPB = 4;
+        const PERLIN_YWRAP = 1 << PERLIN_YWRAPB;
+        const PERLIN_ZWRAPB = 8;
+        const PERLIN_ZWRAP = 1 << PERLIN_ZWRAPB;
+
+        let perlin = new Array(PERLIN_SIZE + 1);
+        for (let i = 0; i < PERLIN_SIZE + 1; i++) {
+            seed = this.dumbSeedRandom(seed);
+            perlin[i] = seed
+        }
+    
+        if (x < 0) {
+            x = -x;
+        }
+        if (y < 0) {
+            y = -y;
+        }
+        if (z < 0) {
+            z = -z;
+        }
+    
+        let xi = Math.floor(x),
+        yi = Math.floor(y),
+        zi = Math.floor(z);
+        let xf = x - xi;
+        let yf = y - yi;
+        let zf = z - zi;
+        let rxf, ryf;
+    
+        let r = 0;
+        let ampl = 0.5;
+    
+        let n1, n2, n3;
+    
+        for (let o = 0; o < perlin_octaves; o++) {
+        let of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+    
+        rxf = scaled_cosine(xf);
+        ryf = scaled_cosine(yf);
+    
+        n1 = perlin[of & PERLIN_SIZE];
+        n1 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n1);
+        n2 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+        n2 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+        n1 += ryf * (n2 - n1);
+    
+        of += PERLIN_ZWRAP;
+        n2 = perlin[of & PERLIN_SIZE];
+        n2 += rxf * (perlin[(of + 1) & PERLIN_SIZE] - n2);
+        n3 = perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+        n3 += rxf * (perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+        n2 += ryf * (n3 - n2);
+    
+        n1 += scaled_cosine(zf) * (n2 - n1);
+    
+        r += n1 * ampl;
+        ampl *= perlin_amp_falloff;
+        xi <<= 1;
+        xf *= 2;
+        yi <<= 1;
+        yf *= 2;
+        zi <<= 1;
+        zf *= 2;
+    
+        if (xf >= 1.0) {
+            xi++;
+            xf--;
+        }
+        if (yf >= 1.0) {
+            yi++;
+            yf--;
+        }
+        if (zf >= 1.0) {
+            zi++;
+            zf--;
+        }
+        }
+        return r;
+
     }
 }
 
