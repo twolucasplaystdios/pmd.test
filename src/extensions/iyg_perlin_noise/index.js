f407b7b6c78eacab674b000055070d1892756272
Ianyourgod <ianlouishawthorne@gmail.com>
2023-06-26 14:23:31 -0500

    testing a different seed random

--- src/extensions/iyg_perlin_noise/index.js
+++ src/extensions/iyg_perlin_noise/index.js
@@ -65,14 +65,8 @@ class iygPerlin {
         };
     }
 
-    dumbSeedRandom(max, min) {
-        max = max || 1;
-        min = min || 0;
-    
-        this.seed = (this.seed * 9301 + 49297) % 233280;
-        var rnd = this.seed / 233280;
-    
-        return min + rnd * (max - min);
+    dumbSeedRandom(seed) {
+        return(seed=1e4*Math.sin(seed))-Math.floor(seed)
     }
 
     GetNoise(args, util) {
