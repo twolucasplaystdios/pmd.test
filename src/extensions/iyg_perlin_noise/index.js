fcc790378557727b39127caad1f0039aea86899a
Ianyourgod <ianlouishawthorne@gmail.com>
2023-06-25 21:02:29 -0500

    perlin noise maybe work now

--- src/extensions/iyg_perlin_noise/index.js
+++ src/extensions/iyg_perlin_noise/index.js
@@ -66,21 +66,10 @@ class iygPerlin {
     }
 
     dumbSeedRandom(seed) {
-        let str = seed.toString();
-        let h1 = 1779033703, h2 = 3144134277,
-            h3 = 1013904242, h4 = 2773480762;
-        for (let i = 0, k; i < str.length; i++) {
-            k = str.charCodeAt(i);
-            h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
-            h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
-            h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
-            h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
-        }
-        h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
-        h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
-        h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
-        h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
-        return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+        let x = (171 * seed) % 30269
+        let y = (172 * seed) % 30307
+        let z = (170 * seed) % 30323
+        return (x/30269.0 + y/30307.0 + z/30323.0) % 1.0
     }
 
     GetNoise(args, util) {
@@ -105,6 +94,7 @@ class iygPerlin {
                 console.log(seed)
                 this.perlin[i] = seed
             }
+            this.seed = seed;
         }
 
         
