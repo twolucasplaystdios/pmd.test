ea9e2038c3f8c7824bf3b1e171d2760dd113b644
Ianyourgod <ianlouishawthorne@gmail.com>
2023-06-26 14:46:40 -0500

    testing a new function for random number generation

--- src/extensions/iyg_perlin_noise/index.js
+++ src/extensions/iyg_perlin_noise/index.js
@@ -66,8 +66,17 @@ class iygPerlin {
     }
 
     dumbSeedRandom() {
-        this.seed = (this.seed * 9301 + 49297) % 233280;
-        return this.seed / 233280;
+        let m_w = seed;
+        let m_z = 987654321;
+        let mask = 0xffffffff;
+        m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
+        m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
+        let result = ((m_z << 16) + m_w) & mask;
+        result /= 4294967296;
+        this.seed = result;
+        return result + 0.5;
+        
+        
     }
 
     GetNoise(args, util) {
