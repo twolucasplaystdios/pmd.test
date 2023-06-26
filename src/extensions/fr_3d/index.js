420fb70843adf9b776873c033a785f7bc4853b03
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-06-26 17:01:58 -0500

    whoops i forgor

---
+++ src/extensions/fr_3d/index.js
@@ -0,0 +1,143 @@
+const formatMessage = require('format-message');
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+const PHYSICS = require('physijs');
+const _3d = vm.runtime.ext_jg3d;
+const Three = _3d.three;
+
+
+/**
+ * Class for 3d++ blocks
+ * @constructor
+ */
+class Fr3DBlocks {
+    constructor(runtime) {
+        /**
+         * The runtime instantiating this block package.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
+
+    /**
+     * @returns {object} metadata for this extension and its blocks.
+     */
+    getInfo() {
+        return {
+            autoLoad: true,
+            id: 'jwProto',
+            name: 'Labels', // change this back if you update the extension to have more things
+            // blockIconURI: blockIconURI,
+            color1: '#ffdc7a',
+            color2: '#ffd45e',
+            blocks: [
+                {
+                    opcode: 'labelHat',
+                    text: formatMessage({
+                        id: 'jwProto.blocks.labelHat',
+                        default: 'label [LABEL]',
+                        description: 'Label for some unused blocks.'
+                    }),
+                    disableMonitor: true,
+                    blockType: BlockType.HAT,
+                    arguments: {
+                        LABEL: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "label"
+                        }
+                    }
+                },
+                {
+                    opcode: 'labelFunction',
+                    text: formatMessage({
+                        id: 'jwProto.blocks.labelFunction',
+                        default: 'label [LABEL]',
+                        description: 'Label for some blocks.'
+                    }),
+                    disableMonitor: true,
+                    blockType: BlockType.COMMAND,
+                    branchCount: 1,
+                    hideFromPalette: true,
+                    arguments: {
+                        LABEL: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "label"
+                        }
+                    }
+                },
+                {
+                    opcode: 'labelCommand',
+                    text: formatMessage({
+                        id: 'jwProto.blocks.labelCommand',
+                        default: 'label [LABEL]',
+                        description: 'Label for labeling.'
+                    }),
+                    disableMonitor: true,
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        LABEL: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "label"
+                        }
+                    }
+                },
+                {
+                    opcode: 'labelReporter',
+                    text: formatMessage({
+                        id: 'jwProto.blocks.labelReporter',
+                        default: 'label [LABEL] [VALUE]',
+                        description: 'Label for a value.'
+                    }),
+                    disableMonitor: true,
+                    blockType: BlockType.REPORTER,
+                    arguments: {
+                        LABEL: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "label"
+                        },
+                        VALUE: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "value"
+                        }
+                    }
+                },
+                {
+                    opcode: 'labelBoolean',
+                    text: formatMessage({
+                        id: 'jwProto.blocks.labelBoolean',
+                        default: 'label [LABEL] [VALUE]',
+                        description: 'Label for a boolean.'
+                    }),
+                    disableMonitor: true,
+                    blockType: BlockType.BOOLEAN,
+                    arguments: {
+                        LABEL: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "label"
+                        },
+                        VALUE: {
+                            type: ArgumentType.BOOLEAN
+                        }
+                    }
+                }
+            ]
+        };
+    }
+
+    labelHat() {
+        return false;
+    }
+    labelFunction(args) {
+        args.substack1();
+    }
+    labelCommand() {
+    }
+    labelReporter(args) {
+        return args.VALUE;
+    }
+    labelBoolean(args) {
+        return args.VALUE;
+    }
+}
+
+module.exports = jwProto;
