81956214e1a0d6a21e7caa7305c23b0b18a39584
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-06-26 23:08:06 -0500

    big fixes again

--- src/extensions/fr_3d/index.js
+++ src/extensions/fr_3d/index.js
@@ -22,43 +22,6 @@ class Fr3DBlocks {
         this.Three = function(){return null}
         this.Ammo = Ammovar;
         if(!vm.runtime.ext_jg3d){vm.extensionManager.loadExtensionURL('jg3d').then(()=>{this._3d = vm.runtime.ext_jg3d;this.Three = this._3d.three;})}else{this._3d = vm.runtime.ext_jg3d;this.Three = this._3d.three}
-        this.animate = function(){
-            requestAnimationFrame(function(){this.world.stepSimulation(1 / 60, 10)});
-        }
-        this.enablePhysicsForObjectByName = function(objectName) {
-            var object = this._3d.scene.getObjectByName(objectName);
-
-            if (!!object) {
-
-                var collisionShape = new this.Ammo.btBoxShape(new this.Ammo.btVector3(object.scale.x / 2, object.scale.y / 2, object.scale.z / 2));
-
-                var mass = 1; 
-                var startTransform = new this.Ammo.btTransform();
-                startTransform.setIdentity();
-                var localInertia = new this.Ammo.btVector3(0, 0, 0);
-                collisionShape.calculateLocalInertia(mass, localInertia);
-                startTransform.setOrigin(new this.Ammo.btVector3(object.position.x, object.position.y, object.position.z));
-                var motionState = new this.Ammo.btDefaultMotionState(startTransform);
-                var rbInfo = new this.Ammo.btRigidBodyConstructionInfo(mass, motionState, collisionShape, localInertia);
-                var rigidBody = new this.Ammo.btRigidBody(rbInfo);
-
-                this.world.addRigidBody(rigidBody);
-
-                object.onBeforeRender = function () {
-                var transform = new this.Ammo.btTransform();
-                rigidBody.getMotionState().getWorldTransform(transform);
-                var origin = transform.getOrigin();
-                object.position.set(origin.x(), origin.y(), origin.z());
-                };
-            }
-        }
-        this.disablePhysicsForObjectByName = function(objectName){
-            var object = this._3d.scene.getObjectByName(objectName);
-            if (!!object) {
-                this.world.removeRigidBody(object.userData.rigidBody);
-                object.onBeforeRender = null;
-            }
-        };
     }
     /**
      * @returns {object} metadata for this extension and its blocks.
@@ -100,7 +63,43 @@ class Fr3DBlocks {
             ]
         };
     }
-    setup() {
+    animate() {
+        requestAnimationFrame(function(){this.world.stepSimulation(1 / 60, 10)});
+    }
+    addp(){
+        var object = this._3d.scene.getObjectByName(objectName);
+        if (!!object) {
+
+            var collisionShape = new this.Ammo.btBoxShape(new this.Ammo.btVector3(object.scale.x / 2, object.scale.y / 2, object.scale.z / 2));
+
+            var mass = 1; 
+            var startTransform = new this.Ammo.btTransform();
+            startTransform.setIdentity();
+            var localInertia = new this.Ammo.btVector3(0, 0, 0);
+            collisionShape.calculateLocalInertia(mass, localInertia);
+            startTransform.setOrigin(new this.Ammo.btVector3(object.position.x, object.position.y, object.position.z));
+            var motionState = new this.Ammo.btDefaultMotionState(startTransform);
+            var rbInfo = new this.Ammo.btRigidBodyConstructionInfo(mass, motionState, collisionShape, localInertia);
+            var rigidBody = new this.Ammo.btRigidBody(rbInfo);
+
+            this.world.addRigidBody(rigidBody);
+
+            object.onBeforeRender = function () {
+            var transform = new this.Ammo.btTransform();
+            rigidBody.getMotionState().getWorldTransform(transform);
+            var origin = transform.getOrigin();
+            object.position.set(origin.x(), origin.y(), origin.z());
+            };
+        }
+    }
+    rmp(name){
+        var object = this._3d.scene.getObjectByName(name);
+        if (object) {
+            this.world.removeRigidBody(object.userData.rigidBody);
+            object.onBeforeRender = null;
+        }
+    }
+    setupworld() {
         var collisionConfiguration = this.Ammo.btDefaultCollisionConfiguration();
         var dispatcher = new this.Ammo.btCollisionDispatcher(collisionConfiguration);
         var overlappingPairCache = new this.Ammo.btDbvtBroadphase();
@@ -113,13 +112,18 @@ class Fr3DBlocks {
         );
         this.world.setGravity(new this.Ammo.btVector3(0, -9.8, 0));
     }
+    setup(){
+        setupworld();
+    }
     enablep(args) {
-        this.enablePhysicsForObjectByName(Cast.toString(args.NAME1));
+        addp(Cast.toString(args.NAME1));
     }
     disablep(args) {
-        this.disablePhysicsForObjectByName(Cast.toString(args.NAME1))
+        rmp(Cast.toString(args.NAME1));
     }
-    step() {this.animate();}
+    step() {
+        animate();
+    }
 }
 
 module.exports = Fr3DBlocks;
