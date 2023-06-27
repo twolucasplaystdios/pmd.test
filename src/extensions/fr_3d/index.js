87b4a5dc5e5a1d12e22a25ce1b3908d0c72d401e
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-06-27 12:20:24 -0500

    should work maybe?

--- src/extensions/fr_3d/index.js
+++ src/extensions/fr_3d/index.js
@@ -2,26 +2,23 @@ const formatMessage = require('format-message');
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const Cast = require('../../util/cast');
-const Ammoconst = require('ammojs3');
+const Ammo = require('three/examples/jsm/libs/Ammo().wasm');
 const Icon = require('./icon.png');
 
 
 
 /**
- * Class for 3d++ blocks
- * @constructor
+ * Class for 3d Physics blocks
  */
 class Fr3DBlocks {
     constructor(runtime) {
         /**
          * The runtime instantiating this block package.
-         * @type {Runtime}
          */
         this.runtime = runtime;
         this.world = {}
         this._3d = {}
         this.Three = {}
-        this.Ammo = Ammoconst;
         if (!vm.runtime.ext_jg3d) {
             vm.extensionManager.loadExtensionURL('jg3d')
                 .then(() => {
@@ -34,10 +31,10 @@ class Fr3DBlocks {
         }
     }
     /**
-     * @returns {object} metadata for this extension and its blocks.
+     * metadata for this extension and its blocks.
+     * @returns {object}
      */
     getInfo() {
-        
         return {
             id: 'fr3d',
             name: '3d Physics',
@@ -75,58 +72,62 @@ class Fr3DBlocks {
         };
     }
     animate () {
+        Ammo().then(function(){
         requestAnimationFrame(() => {
             this.world.stepSimulation(1 / 60, 10)
-        });
+        })})
     }
     addp (objectName) {
+        Ammo().then(function(){
         if (this._3d.scene) {
             const object = this._3d.scene.getObjectByName(objectName);
             if (object) {
 
-            const collisionShape = new this.Ammo.btBoxShape(new this.Ammo.btVector3(object.scale.x / 2, object.scale.y / 2, object.scale.z / 2));
+            const collisionShape = new Ammo().btBoxShape(new Ammo().btVector3(object.scale.x / 2, object.scale.y / 2, object.scale.z / 2));
 
             const mass = 1; 
-            const startTransform = new this.Ammo.btTransform();
+            const startTransform = new Ammo().btTransform();
             startTransform.setIdentity();
-            const localInertia = new this.Ammo.btVector3(0, 0, 0);
+            const localInertia = new Ammo().btVector3(0, 0, 0);
             collisionShape.calculateLocalInertia(mass, localInertia);
-            startTransform.setOrigin(new this.Ammo.btVector3(object.position.x, object.position.y, object.position.z));
-            const motionState = new this.Ammo.btDefaultMotionState(startTransform);
-            const rbInfo = new this.Ammo.btRigidBodyConstructionInfo(mass, motionState, collisionShape, localInertia);
-            const rigidBody = new this.Ammo.btRigidBody(rbInfo);
+            startTransform.setOrigin(new Ammo().btVector3(object.position.x, object.position.y, object.position.z));
+            const motionState = new Ammo().btDefaultMotionState(startTransform);
+            const rbInfo = new Ammo().btRigidBodyConstructionInfo(mass, motionState, collisionShape, localInertia);
+            const rigidBody = new Ammo().btRigidBody(rbInfo);
 
             this.world.addRigidBody(rigidBody);
 
             object.onBeforeRender = () => {
-                    const transform = new this.Ammo.btTransform();
+                    const transform = new Ammo().btTransform();
                     rigidBody.getMotionState().getWorldTransform(transform);
                     const origin = transform.getOrigin();
                     object.position.set(origin.x(), origin.y(), origin.z());
                 };
             }
-        }
+        }})
     }
     rmp (name) {
+        Ammo().then(function(){
         const object = this._3d.scene.getObjectByName(name);
         if (object) {
             this.world.removeRigidBody(object.userData.rigidBody);
             object.onBeforeRender = null;
-        }
+        }})
     }
     setupworld () {
-        alert(typeof this.Ammo.btDefaultCollisionConfiguration)
-        const collisionConfiguration = this.Ammo.btDefaultCollisionConfiguration;
-        const dispatcher = new this.Ammo.btCollisionDispatcher(collisionConfiguration);
-        const overlappingPairCache = new this.Ammo.btDbvtBroadphase();
-        const solver = new this.Ammo.btSequentialImpulseConstraintSolver();
-        this.world = new this.Ammo.btDiscreteDynamicsWorld(
+        Ammo().then(function(){
+        alert(typeof Ammo().btDefaultCollisionConfiguration())
+        const collisionConfiguration = Ammo().btDefaultCollisionConfiguration;
+        const dispatcher = new Ammo().btCollisionDispatcher(collisionConfiguration);
+        const overlappingPairCache = new Ammo().btDbvtBroadphase();
+        const solver = new Ammo().btSequentialImpulseConstraintSolver();
+        this.world = new Ammo().btDiscreteDynamicsWorld(
             dispatcher,
             overlappingPairCache,
             solver,
             collisionConfiguration
         );
-        this.world.setGravity(new this.Ammo.btVector3(0, -9.8, 0));
+        this.world.setGravity(new Ammo().btVector3(0, -9.8, 0))})
     }
     setup () {
         this.setupworld();
