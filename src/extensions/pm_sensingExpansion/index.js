9c82b60788bf69b3d6ecad7b8ae50a48d4a12e53
Ianyourgod <ianlouishawthorne@gmail.com>
2023-06-30 19:30:33 -0500

    forgor a let :skull: :skull: :skull: :skull:

--- src/extensions/pm_sensingExpansion/index.js
+++ src/extensions/pm_sensingExpansion/index.js
@@ -464,7 +464,7 @@ class pmSensingExpansion {
 
     deltaTime() {
         let now = Date.now();
-        dt = now - this.lastUpdate;
+        let dt = now - this.lastUpdate;
         this.lastUpdate = now;
         return dt;
     }
