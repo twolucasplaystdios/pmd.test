fe9ad7b102dd1f319bee452b5ad8fd5254e6ea5f
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-07-06 04:45:43 -0600

    Revert "part 1"
    This reverts commit 81ec9321e5f982b7cba3d3a0f6745a5e98d9d29f.

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -14,8 +14,6 @@ const ExtensionManager = require('./extension-support/extension-manager');
 const log = require('./util/log');
 const MathUtil = require('./util/math-util');
 const Runtime = require('./engine/runtime');
-const RenderedTarget = require('./sprites/rendered-target');
-const Sprite = require('./sprites/sprite');
 const StringUtil = require('./util/string-util');
 const formatMessage = require('format-message');
 
@@ -212,15 +210,6 @@ class VirtualMachine extends EventEmitter {
         this.flyoutBlockListener = this.flyoutBlockListener.bind(this);
         this.monitorBlockListener = this.monitorBlockListener.bind(this);
         this.variableListener = this.variableListener.bind(this);
-        
-        /**
-         * Export some internal classes for extensions.
-         */
-        this.exports = {
-            Sprite,
-            RenderedTarget
-        };
-
         this.addListener('workspaceUpdate', () => {
             this.extensionManager.refreshDynamicCategorys();
         });
