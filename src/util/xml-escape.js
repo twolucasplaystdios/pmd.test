8d9229d029b4af96ebb3a13c0ed57df08a9c2e93
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-08-08 01:30:52 -0700

    Update xml-escape.js

--- src/util/xml-escape.js
+++ src/util/xml-escape.js
@@ -15,7 +15,7 @@ const xmlEscape = function (unsafe) {
             // See #1030
             unsafe = String(unsafe);
         } else {
-            log.error('Unexpected input recieved in replaceUnsafeChars');
+            log.error(`Unexptected type ${typeof unsafe} in xmlEscape at: ${new Error().stack}`)
             return unsafe;
         }
     }
