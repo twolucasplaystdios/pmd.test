723f54b0a9bcc71fac94e6863543eb2c21055e53
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-08-09 13:20:18 -0700

    Update color.js

--- src/util/color.js
+++ src/util/color.js
@@ -32,7 +32,7 @@ class Color {
         if (decimal < 0) {
             decimal += 0xFFFFFF + 1;
         }
-        let hex = Number(decimal).toString(16);
+        let hex = Math.round(Number(decimal)).toString(16);
         hex = `#${'000000'.substr(0, 6 - hex.length)}${hex}`;
         return hex;
     }
